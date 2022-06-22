14f6579af5f0e7abf1de9d5989262e5b31a0fd6a
Simon Oladeji <simmax456@gmail.com>
2022-06-22 04:17:42 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// basic arithmetic calculator
+// It should be able to perform basic operations like +, -, *, & /
+
+// ask user for the operator
+const operator = prompt('Enter operator ( +, -, * or / ): '),
+
+// ask user for the operands
+      firstNum = parseFloat(prompt('Enter first number: ')),
+      secondNum = parseFloat(prompt('Enter second number: '));
+
+let result, buffer;
+
+// calculation operations
+if (operator == '+') {
+    result = firstNum + secondNum;
+}
+else if (operator == '-') {
+    result = firstNum - secondNum;
+}
+else if (operator == '*') {
+    result = firstNum * secondNum;
+}
+else if (operator == '/') {
+    result = firstNum / secondNum;
+}
+else {
+    prompt("Invalid operator")
+}
+
+// display the result
+alert(`${firstNum} ${operator} ${secondNum} = ${result}`);
+console.log(`${firstNum} ${operator} ${secondNum} = ${result}`);
